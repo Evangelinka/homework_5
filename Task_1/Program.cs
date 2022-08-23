dc1c1b07d8dad9ec5fa6513978587f300ca74bbd
«Kristina» <kristinabol1996@gmail.com>
2022-08-24 01:31:28 +0800

    1 задача, четные числа в массиве

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+void FindEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    Console.WriteLine($"Количество четных чисел в массиве = {count} элементов");
+}
+
+int[] array = CreateArray(15, 100, 999);
+PrintArray(array);
+FindEvenNumbers(array);
